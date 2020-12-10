cc26c68b67ffa9df2d920a5b5f95d0fe657ca870
Krunal <krunalvasoya@gmail.com>
2020-12-10 23:32:06 +0530

    demo flask app added

--- flask_app.py
+++ flask_app.py
@@ -11,5 +11,13 @@ model = tf.keras.models.load_model('E:\PROJECT\Banknote-Authentication\model.h5'
 def welcome():
     return "Welcome to Bank Note Authentication App"
 
+@app.route('/predict')
+def predict():
+    variance=request.args.get('variance')
+    skewness=request.args.get('skewness')
+    curtosis=request.args.get('curtosis')
+    entropy=request.args.get('entropy')
+    prediction=np.argmax(model.predict([[float(variance),float(skewness),float(curtosis),float(entropy)]]), axis=-1) 
+    return "Predicted Class : "+ str(prediction[0])
 if __name__=='__main__':
     app.run()
